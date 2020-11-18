1258d7ea96349da74ed3efe6ba3f9e34dd0d9f7c
Victor Ortiz <ortiz.victor.w@gmail.com>
2020-11-17 23:44:44 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -12,13 +13,14 @@ var usersRouter = require('./routes/users');
 require('dotenv').config({path:'variables.env'})
 
 var app = express();
+
 connect();
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
